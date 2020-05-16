f8c1632bb3b47e68091156771771c642daeeec9b
dream2u6 <dream2u6@gmail.com>
2020-05-17 04:12:25 +0700

    Added 1st. commit

---
+++ Exercise4_Sittiphon_S.py
@@ -0,0 +1,16 @@
+fe = 96
+gb = 87
+ic = 84
+cp = 100
+tl = 1000
+print("หมาลัย ปักเป้าใส่กลอน")
+print("คะแนนสอบวัดผลการศึกษา")
+print("นาย สิทธิพล ศรียุกตศุทธ")
+print("ได้คะแนนตามรายวิชาดังต่อไปนี้")
+print("********************************************")
+print("Foundation English               ",fe," คะแนน")
+print("General Business                 ",gb," คะแนน")
+print("Introduction to Computer Systems ",ic," คะแนน")
+print("Computer Programming             ",cp,"คะแนน")
+print("********************************************")
+print("หมายเหตุ แต่ละรายวิชามีคะแนนเต็ม ",tl," คะแนน")
