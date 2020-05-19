9c67891255d4056fc1f1fa880a53838946f5a387
dream2u6 <dream2u6@gmail.com>
2020-05-20 05:31:56 +0700

    Update EX8

--- Exercise8_Sittiphon_S.py
+++ Exercise8_Sittiphon_S.py
@@ -2,19 +2,37 @@ usernameInput = input("Username : ")
 passwordInput = input("password : ")
 if usernameInput == "admin" and passwordInput == "1234" :
     print("Done !")
-else :
-    print("Error T_T")
-print("*********Welcome Bkk Service*********")
-print("ram = 50")
-print("mouse = 20")
-print("monitor = 100")
-print("keyboard = 20")
-print("loudspeaker = 10")
-item = int(input("item price :"))
-quantity = int(input("how many item :"))
-print("------------------------------------")
-print("TOTAL : ",item*quantity)
-print("****Thank you ples visit us again****")
+    print("*********Welcome Bkk Service*********")
+    print("1. ram")
+    print("2. mouse")
+    print("3. monitor")
+    print("4. keyboard")
+    print("5. loudspeaker")
+    item = int(input("select number item :"))
+    quantity = int(input("how many item :"))
+    ram = 50
+    mouse = 20
+    monitor = 100
+    keyboard = 20
+    loudspeaker = 10
+    print("------------------------------------")
+    if item == 1:
+        print("TOTAL : ", ram*quantity)
+    elif item == 2:
+        print("TOTAL : ", mouse*quantity)
+    elif item == 3:
+        print("TOTAL : ", monitor*quantity)
+    elif item == 4:
+        print("TOTAL : ", keyboard*quantity)
+    elif item == 5:
+        print("TOTAL : ", loudspeaker*quantity)
+
+    print("------------------------------------")
+
+    print("****Thank you ples visit us again****")
+else:
+    print("Error")
+
 
 
 
