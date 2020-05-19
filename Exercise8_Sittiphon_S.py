c94a773725954a324a16a1df600930ce9d66126f
dream2u6 <dream2u6@gmail.com>
2020-05-20 04:12:31 +0700

    Home work8

---
+++ Exercise8_Sittiphon_S.py
@@ -0,0 +1,20 @@
+usernameInput = input("Username : ")
+passwordInput = input("password : ")
+if usernameInput == "admin" and passwordInput == "1234" :
+    print("Done !")
+else :
+    print("Error T_T")
+print("*********Welcome Bkk Service*********")
+print("ram = 50")
+print("mouse = 20")
+print("monitor = 100")
+print("keyboard = 20")
+print("loudspeaker = 10")
+item = int(input("item price :"))
+quantity = int(input("how many item :"))
+print("------------------------------------")
+print("TOTAL : ",item*quantity)
+print("****Thank you ples visit us again****")
+
+
+
