4e73bd74b4aa4ad920a372a78a040ca792155791
dream2u6 <dream2u6@gmail.com>
2020-05-23 03:51:54 +0700

    Lecture54

---
+++ Lecture54.py
@@ -0,0 +1,32 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        print("Wrong Username or Password")
+        return login()
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+login()
+showMenu()
+print("**********************************")
+print("Total Price + vat include ; ",priceCalculator())
