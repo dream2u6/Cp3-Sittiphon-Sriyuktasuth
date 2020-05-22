df5b71b6b50a169342cda3c619e2e03740a22855
dream2u6 <dream2u6@gmail.com>
2020-05-23 02:35:00 +0700

    updateLt50

---
+++ Lecture50_Sittiphon_S.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(10,20)
+def addNumber(x,y):
+    print(x-y)
+addNumber(50,20)
+def addNumber(x,y):
+    print(x*y)
+addNumber(15,2)
+def addNumber(x,y):
+    print(x/y)
+addNumber(150,5)
